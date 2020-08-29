004ad2701e0e699683f3a2cf1b14b2b06de4f24c
Dillon Lomnitzer <yodaesu@gmail.com>
2020-08-29 08:38:18 -0400

    Message test case

--- messageservice/cmd/messageservice_test.go
+++ messageservice/cmd/messageservice_test.go
@@ -1,14 +1,20 @@
 package messageservice
 
 import (
-	// "context"
+	"context"
 	"testing"
 	// "github.com/dlzer/gRPC-skeleton/messageservice/models"
-	// pb "github.com/dlzer/gRPC-skeleton/messageservice/protos/messageservice"
+	pb "github.com/dlzer/gRPC-skeleton/messageservice/protos/messageservice"
 )
 
 func Test_Message_Service(t *testing.T) {
-	if 1 == 1 {
-		t.Error("Test fail")
+
+	server := MessageServer{}
+
+	// Test Case: Attempt to send a simple message
+	_, err := server.Message(context.Background(), &pb.Message{Body: "Hello"})
+	if err != nil {
+		t.Error("1. Server responded with an error: ", err.Error())
 	}
+
 }
